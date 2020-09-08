36a01846b7c5d42b7172b10aa39614a91cf23b4e
changodiez <joaquindiezw@gmail.com>
2020-09-08 21:03:52 +0200

    test db

--- db.js
+++ db.js
@@ -6,12 +6,9 @@ const devConfig = {
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: 5432,
+  port: process.env.PG_PORT,
 }
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL
-}
 
 const pool = new Pool( devConfig) ;
 
